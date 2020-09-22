7de79f884bcbafc9f3f8e91601e3191ab65dd580
Oralita <oat431@gmail.com>
2020-09-22 23:12:19 +0700

    move the directory to Lab01

--- 1.4.7.1/src/model/Character/BattleMageCharacter.java
+++
@@ -1,5 +0,0 @@
-package model.Character;
-
-public class BattleMageCharacter {
-    
-}
--- 1.4.7.6/src/model/DamageType.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public enum DamageType {
-    physical,
-    magical,
-    both // add both type for reference in 1.4.7.6
-}
